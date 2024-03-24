611eb8e15edb2a657e7a05f804ccbdf5aab96ca7
Wolf Bublitz <wolf.bublitz@gmail.com>
2024-03-24 20:51:27 +0100

    added LogMessageWriterTests tests

---
+++ tests/LogMessageWriterTests/src/SmartConsoleInjectionTest.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using L0gg3r;
+using L0gg3r.LogSinks.Console.SmartConsoleLogSink;
+
+namespace LogMessageWriterTests.SmartConsoleInjectionTest;
+
+[LogMessageWriter<SmartConsoleLogSink, int>]
+internal class LogMessageWriter : ILogMessageWriter
+{
+    public LogMessageWriter(SmartConsole smartConsole)
+    {
+        SmartConsole = smartConsole;
+    }
+
+    public static SmartConsole SmartConsole { get; set; }
+
+    public ValueTask<Continuation> WriteAsync(in LogMessage logMessage)
+    {
+        return ValueTask.FromResult(Continuation.Stop);
+    }
+}
+
+[TestClass]
+public class TheSmartConsoleLogSink
+{
+    [TestMethod]
+    public async Task ShallInjectTheSmartConsoleIntoTheLogMessageWriter()
+    {
+        // Arrange
+        SmartConsoleLogSink logSink = new();
+        logSink.ServiceProvider.RegisterLogMessageWriter<LogMessageWriter>();
+
+        // Act
+        await logSink.SubmitAsync(new LogMessage() { Payload = 42 }).ConfigureAwait(false);
+        await logSink.FlushAsync().ConfigureAwait(false);
+
+        // Assert
+        LogMessageWriter.SmartConsole.Should().NotBeNull();
+    }
+}
