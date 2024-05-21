b0decc225cd630ce49e17a7036181ef557d19f1c
Wolf Bublitz <wolf.bublitz@gmail.com>
2024-05-21 06:37:02 +0200

    added exception payload output test

--- tests/OutputTests/src/PayloadOutputTests.cs
+++ tests/OutputTests/src/PayloadOutputTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using L0gg3r.Base;
 
@@ -20,6 +21,38 @@ public class TheSmartConsoleLogSink : VerifyBase
             await logSink.FlushAsync().ConfigureAwait(false);
 
             consoleOutput = testConsole.Output;
+        }
+
+        string output = consoleOutput.ScrubDateTime("yyyy-MM-dd HH\\:mm\\:ss.ffffff", replacement: "Timestamp");
+
+        // Assert
+        await Verify(output);
+    }
+
+    [TestMethod]
+    public async Task ShouldWriteALogMessageWithAExceptionPayloadToConsole()
+    {
+        // Arrange
+        SmartConsoleLogSink logSink = new();
+        string consoleOutput;
+        LogMessage logMessage = new();
+
+        static void action() => throw new Exception("Some error");
+
+        try
+        {
+            action();
+        }
+        catch (Exception exception)
+        {
+            // Act
+            using (TestConsole testConsole = new())
+            {
+                await logSink.SubmitAsync(new LogMessage() { Payload = exception, }).ConfigureAwait(false);
+                await logSink.FlushAsync().ConfigureAwait(false);
+
+                consoleOutput = testConsole.Output;
+            }
         }
 
         string output = consoleOutput.ScrubDateTime("yyyy-MM-dd HH\\:mm\\:ss.ffffff", replacement: "Timestamp");
